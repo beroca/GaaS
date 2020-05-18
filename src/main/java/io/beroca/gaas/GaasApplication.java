b3c9e05bda0968d912f9b8d705fa322db78ba67e
Bene Rodriguez <benedicto.rodriguez.castro@sap.com>
2020-05-18 18:35:25 +0200

    gaas-1 update GaasApplication javadoc

--- src/main/java/io/beroca/gaas/GaasApplication.java
+++ src/main/java/io/beroca/gaas/GaasApplication.java
@@ -3,6 +3,9 @@ package io.beroca.gaas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * javadoc
+ */
 @SpringBootApplication
 public class GaasApplication {
 
